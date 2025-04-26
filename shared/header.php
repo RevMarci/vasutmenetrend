e05e279445ae3c999b9bb7575aeb0d9e0c41ed2d
rbrt1022 <rbrt1022.it@outlook.hu>
2025-04-26 17:19:55 +0200

    jegyvásárlás opció hozzáadva (csak bejelentkezve jelenik meg)

--- shared/header.php
+++ shared/header.php
@@ -12,9 +12,12 @@ require_once ROOT_PATH . 'config.php';
     <a href="<?= BASE_URL ?>pages/stations.php">Állomások</a>
 
     <?php
+    //$_SESSION['login'] = '';
     if ($_SESSION['login'] === 'tag') {
+      echo "<a href='" . BASE_URL . "pages/jegyvasarlas.php'>Jegyvásárlás</a>";
       echo "<a class='blackButton' href='" . BASE_URL . "pages/profile.php'>Profil</a>";
     } elseif ($_SESSION['login'] === 'admin') {
+      echo "<a href='" . BASE_URL . "pages/jegyvasarlas.php'>Jegyvásárlás</a>";
       echo "<a class='blackButton' href='" . BASE_URL . "pages/admin.php'>Admin</a>";
     } else {
       echo "<a class='blackButton' href='" . BASE_URL . "pages/login.php'>Bejelentkezés</a>";
